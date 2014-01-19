e362833d6838de534735c9475819a54b32631640
Seiji Nitta <zenith6@gmail.com>
2014-01-20 03:53:50 +0900

    プラグインが無効な場合にプラグイン設定画面が開けない不具合を修正

--- src/pages/plg_Devel_LC_Page_Config.php
+++ src/pages/plg_Devel_LC_Page_Config.php
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+require_once PLUGIN_UPLOAD_REALDIR . 'Devel/Devel.php';
 require_once CLASS_EX_REALDIR . 'page_extends/admin/LC_Page_Admin_Ex.php';
 
 /**
